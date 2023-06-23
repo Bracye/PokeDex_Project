0029308011142d30af45c40e45cf1f2d2d58134b
Ernest Bracy <ernestbracy1@gmail.com>
2023-06-23 10:29:20 -0400

    made a successfull call to PokeAPI

--- script.js
+++ script.js
@@ -18,11 +18,18 @@ const colors = {
   normal: "#F5F5F5",
 };
 
-const fetchPokemons = () => {
+const fetchPokemons = async () => {
   for (let i = 1; i <= pokemon_count; i++) {
     console.log(i);
-    // await getPokemon(i);
+    await getPokemon(i);
   }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
 };
 
 fetchPokemons();
